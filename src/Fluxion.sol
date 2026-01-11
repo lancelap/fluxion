61dcb767368b0604460b6982fa7914790ed14d4b
Daniel Potapov <lancelap@icloud.com>
2026-01-11 13:26:01 +0700

    fix: update import path for Initializable contract

--- src/Fluxion.sol
+++ src/Fluxion.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.33;
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {AccessControlEnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract Fluxion is
