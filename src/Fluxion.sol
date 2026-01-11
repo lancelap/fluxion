6a3d536b20aa3b58371894664185cca76f61b243
Daniel Potapov <lancelap@icloud.com>
2026-01-11 16:08:22 +0700

    fix: update import paths for Initializable and UUPSUpgradeable contracts

--- src/Fluxion.sol
+++ src/Fluxion.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.33;
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {AccessControlEnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract Fluxion is
     Initializable,
@@ -34,9 +34,6 @@ contract Fluxion is
     _grantRole(DEFAULT_ADMIN_ROLE, admin_);
     _grantRole(MINTER_ROLE, admin_);
     _grantRole(UPGRADER_ROLE, admin_);
-
-    // init mint some tokens to admin
-    _mint(admin_, 1_000_000 * 10 ** decimals());
    }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
